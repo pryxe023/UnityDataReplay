ab14d97d6d967ef903297136fa27416bd1ee3466
pryxe023 <91890236+pryxe023@users.noreply.github.com>
2021-10-08 15:32:38 +0200

    First basic attempt.

---
+++ ObjectReplay/ObjectReplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class ObjectReplay : MonoBehaviour
+{
+	public GameObject movingObject;
+	public TextAsset csvFile;
+
+	// Update is called once per frame
+	void Update() {
+		readCSV();
+	}
+
+	void readCSV()
+    {
+		string[] records = csvFile.text.Split('\n'); // Get number of lines = number of updates
+
+		for(int i = 1; i < records; i++) // i = 1 as start, since the top line is just a header and doesn't contain positional information
+        {
+			string[] fields = records[i].Split(','); // Separate from commas (Comma Separated Values)
+
+			movingObject.transform.Position(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
+			movingObject.transform.Rotate(float.Parse(fields[4]), float.Parse(fields[5]), float.Parse(fields[6]));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,6 +7,19 @@ Alternatively, use my fork which includes a super simple GameObject Logger (http
 ## How it works
 The recorded data (see above) is in csv-files. This (Re)player allows you to move GameObjects based on the information in these csv-files.
 
-This is still a work in progress.
+**This is still a work in progress.**
 
 :construction:
+
+### CSV format
+It is important that the format of your csv-files is as follows (from left to right);
+
+1. ~Title~
+1. Position X
+1. Position Y
+1. Position Z
+1. Rotation X
+1. Rotation Y
+1. Rotation Z
+1. Rotation W
+1. ~Timestamp~

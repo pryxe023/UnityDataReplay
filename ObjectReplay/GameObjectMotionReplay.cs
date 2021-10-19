c77d03fbf5a823a7f3d125ae2510eac68dba141e
pryxe023 <91890236+pryxe023@users.noreply.github.com>
2021-10-19 14:55:14 +0200

    Fixed replay speed

--- ObjectReplay/GameObjectMotionReplay.cs
+++ ObjectReplay/GameObjectMotionReplay.cs
@@ -6,7 +6,6 @@ public class GameObjectMotionReplay : MonoBehaviour
 {
     private struct Step
     {
-        public float speed;
         public Vector3 destination;
         public Quaternion rotationgoal;
         public float timestamp;
@@ -15,9 +14,7 @@ public class GameObjectMotionReplay : MonoBehaviour
     [Header("Replay settings")] // Input in the Unity inspector
     [Tooltip("Select the csv-file containing the positions and rotations.")]
     [SerializeField] public TextAsset csvFile;
-    [Tooltip("Set the speed to replay the recorded motion.")]
-    [SerializeField] public float speed = 1.5f;
-    [Tooltip("Set a scaling-factor for the positions.")]
+    [Tooltip("Set a scaling-factor for the positions. Recommended to keep at 1.")]
     [SerializeField] public float scalingFactor = 1.0f;
     private List<Step> steps;
     private int currentStep = 0;
@@ -33,14 +30,16 @@ public class GameObjectMotionReplay : MonoBehaviour
 
     void Update()
     {
-        ++currentStep;
+        if (currentStep < steps.Count - 1 && steps[currentStep].timestamp < Time.time)
+        {
+            ++currentStep;
 
-        // Apply the movements and rotations
-        this.transform.position = Vector3.MoveTowards(this.transform.position, steps[currentStep].destination, steps[currentStep].speed * Time.deltaTime);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, steps[currentStep].rotationgoal, steps[currentStep].speed * Time.deltaTime);
+            // Apply the movements and rotations
+            this.transform.SetPositionAndRotation(steps[currentStep].destination, steps[currentStep].rotationgoal);
+        }
 
         // Print the timestamp for debugging
-        Debug.Log(steps[currentStep].timestamp);
+        Debug.Log("Replay time: " + steps[currentStep].timestamp + " | Real time: " + Time.time);
     }
 
     void readCSV()
@@ -54,7 +53,6 @@ public class GameObjectMotionReplay : MonoBehaviour
             steps.Add(
                 new Step()
                 {
-                    speed = speed,
                     destination = new Vector3(float.Parse(array[1]) * scalingFactor, float.Parse(array[2]) * scalingFactor, float.Parse(array[3]) * scalingFactor),
                     rotationgoal = new Quaternion(float.Parse(array[4]), float.Parse(array[5]), float.Parse(array[6]), float.Parse(array[7])),
                     timestamp = float.Parse(array[8])

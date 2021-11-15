9a3052b7803c41c26ad33aad98b2830723d38e83
pryxe023 <91890236+pryxe023@users.noreply.github.com>
2021-11-15 12:29:44 +0100

    Enabled rescaling during runtime
    If you have to scale other objects, you can now also scale the replay to match directly

--- ObjectReplay/ReplayManager.cs
+++ ObjectReplay/ReplayManager.cs
@@ -29,7 +29,6 @@ public class ReplayManager : MonoBehaviour
     private float addedTime = 0.0f;
     private float pauseTime = 0.0f;
     private float playTime = 0.0f;
-    private bool allowReplay = false;
 
     private struct Step
     {
@@ -54,7 +53,7 @@ public class ReplayManager : MonoBehaviour
             allSteps.Add(i,steps);
 
             // Set the GameObject to the correct starting position and rotation
-            replayObjects[i].transform.position = allSteps[i][0].destination;
+            replayObjects[i].transform.position = allSteps[i][0].destination * scalingFactor;
             replayObjects[i].transform.rotation = allSteps[i][0].rotationgoal;
         }
     }
@@ -83,7 +82,7 @@ public class ReplayManager : MonoBehaviour
                 for (int i = 0; i < replayObjects.Length; i++)
                 {
                     // Apply the movements and rotations
-                    replayObjects[i].transform.SetPositionAndRotation(allSteps[i][currentStep].destination, allSteps[i][currentStep].rotationgoal);
+                    replayObjects[i].transform.SetPositionAndRotation(allSteps[i][currentStep].destination * scalingFactor, allSteps[i][currentStep].rotationgoal);
                 }
             }
             if (currentStep == allSteps[0].Count - 1 && currentRep < replayReps)
@@ -111,7 +110,7 @@ public class ReplayManager : MonoBehaviour
                 steps.Add(
                 new Step()
                     {
-                        destination = new Vector3(float.Parse(array[1]) * scalingFactor, float.Parse(array[2]) * scalingFactor, float.Parse(array[3]) * scalingFactor),
+                        destination = new Vector3(float.Parse(array[1]), float.Parse(array[2]), float.Parse(array[3])),
                         rotationgoal = new Quaternion(float.Parse(array[4]), float.Parse(array[5]), float.Parse(array[6]), float.Parse(array[7])),
                         timestamp = float.Parse(array[8])
                     }
